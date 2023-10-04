421d224146688d41644fcd2e0a01126b99ae2d8f
196Sebastian <correa.sebas6@gmail.com>
2023-10-04 14:47:50 +0900

    weather icon changes based on the response from the BE

--- response.js
+++ response.js
@@ -3,6 +3,8 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const userInput = document.querySelector(".search-section input");
 const buttonSearch = document.querySelector(".search-section button");
+const weatherIcon = document.querySelector(".weather-icon");
+
 
 async function checkWeather(city) {
   const response = await fetch(`${apiUrl} ${city} &appid=${apiKey}`);
@@ -12,6 +14,18 @@ async function checkWeather(city) {
   document.querySelector(".wind").innerHTML = `${data.wind.speed} km/h`;
   document.querySelector(".temp").innerHTML = `${Math.round(data.main.temp)}°c`;
   document.querySelector(".humidity").innerHTML = `${data.main.humidity}%`;
+
+  if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "images/clouds.png";
+  } else if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "images/clear.png";
+  } else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "images/rain.png";
+  } else if (data.weather[0].main == "Drizzle") {
+    weatherIcon.src = "images/drizzle.png";
+  } else if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "images/mist.png";
+  }
 }
 
 buttonSearch.addEventListener("click", () => {
