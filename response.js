9654460bd1263b44c0c83cf25bf0323e261f6cca
196Sebastian <correa.sebas6@gmail.com>
2023-10-04 15:17:22 +0900

    hide apikey

--- response.js
+++ response.js
@@ -1,4 +1,4 @@
-const apiKey = "f6480f568f3c6737afa1179ee96aac4d";
+const apiKey = "n/a";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const userInput = document.querySelector(".search-section input");
